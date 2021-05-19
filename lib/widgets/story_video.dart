fab1bbce86ab8b3c26f075bdaae8547e9f7069be
Julian Kalinowski <julian.kalinowski@softiero.com>
2021-05-19 18:21:28 +0200

    downgrade videoplayer version

--- lib/widgets/story_video.dart
+++ lib/widgets/story_video.dart
@@ -116,7 +116,7 @@ class StoryVideoState extends State<StoryVideo> {
   }
 
   Widget getContentView() {
-    if (widget.videoLoader.state == LoadState.success && playerController.value.isInitialized) {
+    if (widget.videoLoader.state == LoadState.success && playerController.value.initialized) {
       if (!mounted) {
         playerController.pause();
       }
